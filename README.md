ea21d8d554771d655fd9eaa2d585836a3d9b2e00
JuanHiguitaR <devjuan06@gmail.com>
2025-06-26 23:27:36 -0400

    Creat readme file and lending_book file and the last file has the menu and functions ready to do

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Mini_Library
 Mini Library by console control
+Hello, everyone this Code its create for solving issues about of how to better organize books and clients.
+
+MiniLibrary Project:
+It's purpose allow registration books and customers, and simulate lending books and returns in a simple way. Do it with functions and basics data structures, not need multiple files to do it.
+-> use functions
+-> organize the code clearly
+-> create a console interface
+FUNCTIONALITY:
+-Book Record:
+    -> Enter: title, author, ISBN
+    -> Save the book as available
+-Customer Record:
+    -> Enter: name, phone number
+    -> Create automatic ID like SOC-001
+-Lend a book:
+    -> Enter ISBN and ID of customer
+    -> Mark the book as borrowed, with customer ID
+-Return book:
+    -> Enter ISBN
+    -> Change status for enable
+-See list of borrowed books:
+    -> Show books on borrowed status
+-Tecnical structure:
+    One File, use while true for Menu. Functions like register_book(), lend_book().
---
+++ lending_Book.py
@@ -0,0 +1,56 @@
+# Create def main to start the aplication. Inside has a Menu.
+def main():
+    while True:
+        match menu():
+            case "1":
+                book_Record()
+            case "2":
+                customer_Record()
+            case "3":
+                lend_book()
+            case "4":
+                return_Book()
+            case "5":
+                borrow_Status()
+            case "0":
+                print("System closing.......🖥️")
+                print("Thanks for used!👌🏻")
+                break
+            case _:
+                print("❌ Invalid option")
+
+
+def menu() -> str:
+    print("📚===Welcome to library system===📚")
+    print(" 1. Register a book")
+    print(" 2. Register a customer")
+    print(" 3. Lending a book")
+    print(" 4. Return a book")
+    print(" 5. Status lending books")
+    print(" 0. Exit")
+    print("📚===============================📚")
+    return input("Enter a valid option (0-5): ")
+
+
+def book_Record():
+    pass
+
+
+def customer_Record():
+    pass
+
+
+def lend_book():
+    pass
+
+
+def return_Book():
+    pass
+
+
+def borrow_Status():
+    pass
+
+
+if __name__ == "main":
+    main()
